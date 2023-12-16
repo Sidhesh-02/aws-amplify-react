0dc4e66b53d1bdc7695624e33b784cbcb32c8189
Sidhesh Bankar <sidheshcreates@gmail.com>
2023-12-16 18:20:27 +0530

    todo

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifyreact": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "awsamplifyreact": {
       "AppClientID": "string",
--- src/App.js
+++ src/App.js
@@ -1,18 +1,122 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import React, { useState, useEffect } from "react";
 import '@aws-amplify/ui-react/styles.css';
-import { View } from "@aws-amplify/ui-react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  Authenticator
+} from "@aws-amplify/ui-react";
+
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
+
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import config from './amplifyconfiguration.json';
+
+Amplify.configure(config);
+
+const client = generateClient();
 
 export default function App() {
+  const [notes, setNotes] = useState([]);
+  
+    useEffect(() => {
+      fetchNotes();
+    }, []);
+  
+    async function fetchNotes() {
+      const clientData = await client.graphql({ query: listNotes });
+      const notesFromclient = clientData.data.listNotes.items;
+      setNotes(notesFromclient);
+    }
+  
+    async function createNote(event) {
+      event.preventDefault();
+      const form = new FormData(event.target);
+      const data = {
+        name: form.get("name"),
+        description: form.get("description"),
+      };
+      await client.graphql({
+        query: createNoteMutation,
+        variables: { input: data },
+      });
+      fetchNotes();
+      event.target.reset();
+    }
+  
+    async function deleteNote({ id }) {
+      const newNotes = notes.filter((note) => note.id !== id);
+      setNotes(newNotes);
+      await client.graphql({
+        query: deleteNoteMutation,
+        variables: { input: { id } },
+      });
+    }
   return (
-    <View backgroundColor={{base: "grey"}} padding = {{base: "1rem", large:"6rem"}}>
-      <Authenticator loginMechanisms={['email']}>
-        {({ signOut, user }) => (
-          <main>
-            <h1 className="theme">Hello {user.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
-    </View>
+    <Authenticator className="pt-10" loginMechanisms={['email']}>
+      {({ signOut, user }) => (
+        <div className="h-screen">
+          <View className="p-4">
+              <span className="uppercase">Welcome Back {user.username}</span>
+              <Heading level={2}>Create Notes</Heading>
+              <View as="form" margin="3rem" onSubmit={createNote}>
+                <Flex direction="row" justifyContent="center">
+                  <TextField
+                    name="name"
+                    placeholder="Note Name"
+                    label="Note Name"
+                    labelHidden
+                    variation="quiet"
+                    required
+                  />
+                  <TextField
+                    name="description"
+                    placeholder="Note Description"
+                    label="Note Description"
+                    labelHidden
+                    variation="quiet"
+                    required
+                  />
+                  <Button type="submit" variation="primary">
+                    Create Note
+                  </Button>
+                </Flex>
+              </View>
+              <Heading level={2}>Current Notes</Heading>
+              <View margin="3rem 0">
+                {notes.map((note) => (
+                  <Flex
+                    padding="0rem 1rem"
+                    key={note.id || note.name}
+                    direction="row"
+                    justifyContent="left"
+                    alignItems="center"
+                  >
+                    <Text as="strong" fontWeight={700}>
+                      {note.name}
+                    </Text>
+                    <Text as="span">{note.description}</Text>
+                    <Button variation="link" onClick={() => deleteNote(note)}>
+                      Delete note
+                    </Button>
+                  </Flex>
+                ))}
+              </View>
+              <Button onClick={signOut}>Sign Out</Button>
+          </View>
+        </div>
+      )}
+    </Authenticator>
   );
 }
+
+
+
